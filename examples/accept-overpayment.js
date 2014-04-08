528bd758ecfc478cc963a678d520ffbb65f37de1
Gordon Hall <gordon@bitpay.com>
2014-04-08 16:38:38 -0400

    updated examples and fixed a bunch of stuff

---
+++ examples/accept-overpayment.js
@@ -0,0 +1,18 @@
+var fs         = require('fs');
+var KeyUtils   = require('../lib/key-utils');
+var HOME       = process.env['HOME'];
+var BitPay     = require('../lib/rest-client');
+var encPrivkey = fs.readFileSync(HOME + '/.bp/api.key').toString();
+var privkey    = KeyUtils.decrypt('', encPrivkey);
+var client     = new BitPay(privkey);
+
+client.on('ready', function() {
+
+  client.get('invoices/KLydw6yFiZrhSWsqMW6aSk', function(err, invoice) {
+    if (!invoice) return console.log('Invoice not found.');
+    invoice.post('adjustments', { type: 'acceptOverPayment' }, function(err, result) {
+      console.log(err || result);
+    });
+  });
+
+});
