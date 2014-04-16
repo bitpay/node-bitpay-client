8488b549da8bf5422aec71b89d0b263d7eaec5b2
Gordon Hall <gordon@bitpay.com>
2014-04-16 14:22:22 -0400

    add examples for getting ledger entries

--- examples/get-ledger-entries.js
+++ examples/get-ledger-entries.js
@@ -6,15 +6,17 @@ var encPrivkey = fs.readFileSync(HOME + '/.bp/api.key').toString();
 var config     = require('../config');
 var privkey    = KeyUtils.decrypt(config.keyPassword, encPrivkey);
 var client     = new BitPay(privkey);
-var async      = require('async');
 
 client.on('ready', function() {
 
   var today      = new Date().getTime();
   var oneWeekAgo = new Date(today - 1000 * 60 * 60 * 24 * 7).getTime();
 
-  client.get('ledgers', function(err, data) {
-    console.log(err || data);
+  client.get('ledgers/btc', { 
+    startDate: oneWeekAgo,
+    endDate: today
+  }, function(err, data) {
+    console.log(err || data)
   });
 
 });
