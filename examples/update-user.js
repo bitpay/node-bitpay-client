73de6fd6e640b7f43c4c275e3bd99338435e91ee
Gordon Hall <gordon@bitpay.com>
2014-04-17 14:34:23 -0400

    fixed bug with returned user object, added update-user example

---
+++ examples/update-user.js
@@ -0,0 +1,22 @@
+var fs         = require('fs');
+var KeyUtils   = require('../lib/key-utils');
+var HOME       = process.env['HOME'];
+var BitPay     = require('../lib/rest-client');
+var encPrivkey = fs.readFileSync(HOME + '/.bp/api.key').toString();
+var config     = require('../config');
+var privkey    = KeyUtils.decrypt(config.keyPassword, encPrivkey);
+var client     = new BitPay(privkey);
+
+client.on('ready', function() {
+
+  client.as('user').get('user', function(err, user) {
+    console.log(user)
+    user.put({
+      contactPhone: '123-456-7890',
+      contactName: 'Satoshi Nakamoto'
+    }, function(err, user) {
+      console.log(err || user);
+    });
+  });
+
+});
