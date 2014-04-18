70a66700ea9293509a61e1ade3db7a6f95f60bb5
Gordon Hall <gordon@bitpay.com>
2014-04-18 10:50:14 -0400

    added examples for getting exchange rates

--- examples/get-rate-for-currency.js
+++ examples/get-rate-for-currency.js
@@ -1,7 +1,7 @@
 var BitPay     = require('../lib/rest-client');
 var client     = new BitPay();
 
-client.as('public').get('currencies', function(err, data) {
+client.as('public').get('rates/usd', function(err, data) {
   console.log(err || data);
 });
 
